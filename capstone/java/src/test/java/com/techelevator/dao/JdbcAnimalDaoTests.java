dd7cfbea9fa1827db0745d68bf72adf6de27f558
Evan Amezcua <ema9701@gmail.com>
2022-08-08 13:07:46 -0400

    jdbc_tests_fail

--- capstone/java/src/test/java/com/techelevator/dao/JdbcAnimalDaoTests.java
+++ capstone/java/src/test/java/com/techelevator/dao/JdbcAnimalDaoTests.java
@@ -10,6 +10,10 @@ import java.util.List;
 
 public class JdbcAnimalDaoTests extends BaseDaoTests{
 
+    private static final Animal ANIMAL_1 = new Animal(1, "Joe", "Mutt", 7, "A stray from the street", 11);
+    private static final Animal ANIMAL_2 = new Animal(2, "Dorothy", "Calico", 4, "Just a calico cat", 12);
+    private static final Animal ANIMAL_3 = new Animal(3, "Roach", "Pony", 6, "Some horse, I guess", 13);
+
     private JdbcAnimalDao sut;
 
     @Before
@@ -21,7 +25,13 @@ public class JdbcAnimalDaoTests extends BaseDaoTests{
     @Test
     public void returns_animalList() {
         List<Animal> animals = sut.findAll();
-        Assert.assertNotEquals("there should be data", 0, animals.size());
+
+        Assert.assertNotNull(animals);
+        Assert.assertEquals(3, animals.size());
+        Assert.assertEquals(ANIMAL_1, animals.get(0));
+        Assert.assertEquals(ANIMAL_2, animals.get(1));
+        Assert.assertEquals(ANIMAL_3, animals.get(2));
+
     }
 
 }
