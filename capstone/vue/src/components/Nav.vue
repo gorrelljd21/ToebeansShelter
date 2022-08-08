985f6f8b74c45cc649240b9970fdb92e2c77b6be
Jessie Gorrell <gorrelljd21@gmail.com>
2022-08-08 16:00:07 -0400

    navigation

---
+++ capstone/vue/src/components/Nav.vue
@@ -0,0 +1,33 @@
+<template>
+  <nav class="navigation">
+    <h1>Navigation</h1>
+    <button id="animal" type="submit">About Us</button>
+    <br />
+    <button id="animal" type="submit">All Animal Listings</button>
+    <br />
+    <button id="animal" type="submit">Animals by Category</button>
+    <br />
+    <button id="animal" type="submit">Volunteers</button>
+    <br />
+    <button id="animal" type="submit">Success Stories</button>
+    <br />
+    <button id="animal" type="submit">Events</button>
+  </nav>
+</template>
+
+<style scoped>
+.home {
+  display: flex;
+}
+.navigation {
+  text-align: center;
+  border-radius: 8px;
+  border-style: solid;
+  height: 100vh;
+}
+
+#animal {
+  flex-direction: column-reverse;
+  justify-content: space-between;
+}
+</style>
