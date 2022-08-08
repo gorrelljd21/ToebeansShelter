c83ea65a2b255cb8e6277885030b14f6536566c5
Jessie Gorrell <gorrelljd21@gmail.com>
2022-08-08 10:47:16 -0400

    working on endpoints for axios view

---
+++ capstone/vue/src/components/PetDetail.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="loading" v-if="isLoading">
+    <img src="..\assets\3035fb76965389.5c7945b0d0567.gif" />
+  </div>
+</template>
+
+<script>
+import shelterService from "../services/ShelterService";
+
+export default {
+  name: "pet-detail",
+  data() {
+    return {
+      isLoading: true,
+      errorMsg: "",
+    };
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ capstone/vue/src/views/Pet.vue
@@ -0,0 +1,16 @@
+<template>
+  <pet-detail />
+</template>
+
+<script>
+import PetDetail from "@/components/PetDetail";
+
+export default {
+  components: {
+    PetDetail,
+  },
+};
+</script>
+
+<style scoped>
+</style>
