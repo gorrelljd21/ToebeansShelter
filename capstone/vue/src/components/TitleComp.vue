7439be69825efae02608850558c0e01f60d616a4
Jessie Gorrell <gorrelljd21@gmail.com>
2022-08-09 15:20:17 -0400

    header photo

--- capstone/vue/src/components/TitleComp.vue
+++ capstone/vue/src/components/TitleComp.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="title">
-    <h1>TOEBEANS ANIMAL SHELTER</h1>
+    <img src="..\assets\Toebeans Animal Shelter.png" alt="title photo" />
   </div>
 </template>
 
 <style scoped>
-h1 {
-  font-family: Calibri, Candara, Segoe, "Segoe UI", Optima, Arial, sans-serif;
-  font-size: 45px;
-  padding: 35px 0px 0px 78px;
+img {
+  padding: 0% 0% 0% 25%;
+  width: 70%;
+  height: 50%;
 }
 </style>
