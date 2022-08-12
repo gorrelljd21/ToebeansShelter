95abf482eb1da3b5ce59b000cef0e94f46c5f66d
Evan Amezcua <ema9701@gmail.com>
2022-08-12 10:21:33 -0400

    sync

---
+++ capstone/vue/src/components/FormApproval.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="approval-container">
+    <div>
+      <h3>For Pending Volunteers</h3>
+      <button></button>
+      <button></button>
+    </div>
+  </div>
+</template>
+
+<script>
+import ShelterService from "@/services/ShelterService.js";
+
+export default {
+  name: "form-approval",
+  data() {
+    return {
+      volunteers: [],
+    };
+  },
+  methods: {
+    created() {
+      this.getVolunteers();
+    },
+    listVolunteers() {
+      ShelterService.getVolunteers().then((response) => {
+        this.volunteers = response.data;
+      });
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
