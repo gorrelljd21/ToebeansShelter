5a3080700fcdf643a3a94778d5d62cf441ea0557
Jessie Gorrell <gorrelljd21@gmail.com>
2022-08-15 11:53:19 -0400

    style application form view page

--- capstone/vue/src/components/VolunteerApplicationComp.vue
+++ capstone/vue/src/components/VolunteerApplicationComp.vue
@@ -1,32 +1,40 @@
 <template>
   <div>
-    <div>
-      <h2>{{ volunteer.full_name }}</h2>
+    <div class="main">
       <table>
+        <h2>{{ volunteer.full_name }}</h2>
         <tr>
           <th>Phone Number:</th>
           <td>{{ volunteer.phone_number }}</td>
         </tr>
+        <br />
         <tr>
           <th>E-mail:</th>
           <td>{{ volunteer.email }}</td>
         </tr>
+        <br />
         <tr>
           <th>Bio:</th>
           <td>{{ volunteer.bio }}</td>
         </tr>
+        <br />
         <tr>
           <th>Reference Name:</th>
           <td>{{ volunteer.ref_full_name }}</td>
         </tr>
+        <br />
         <tr>
           <th>Reference Phone Number:</th>
           <td>{{ volunteer.ref_phone_number }}</td>
         </tr>
+        <br />
         <tr>
           <th>Reference E-mail:</th>
           <td>{{ volunteer.ref_email }}</td>
         </tr>
+        <button>
+          <router-link id="link" to="/volunteer-requests">Back</router-link>
+        </button>
       </table>
     </div>
   </div>
@@ -67,4 +75,55 @@ export default {
 </script>
 
 <style scoped>
+.main {
+  border: 1px solid black;
+  display: block;
+  height: 100vh;
+  margin: 0px 25px 0px 25px;
+}
+* {
+  font-family: Calibri, Candara, Segoe, "Segoe UI", Optima, Arial, sans-serif;
+}
+
+table {
+  margin-left: 20px;
+  border: 1px solid black;
+  margin: auto;
+  margin-top: 10%;
+  padding: 0px 0px 20px 20px;
+  display: block;
+  border-radius: 10px;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px,
+    rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
+  width: 90%;
+  background-color: #e8e9eb;
+}
+
+th,
+td {
+  background-color: #e8e9eb;
+}
+
+h2 {
+  background-color: #6eb8b6;
+  padding: 5px 5px 5px 12px;
+  border-radius: 3px;
+  margin-right: 40%;
+  color: aliceblue;
+  border: 1px solid black;
+}
+
+button {
+  margin-top: 20px;
+  margin-right: 10px;
+  background-color: #e8e9eb;
+  border-width: 1px;
+  border-radius: 4px;
+}
+
+#link {
+  text-decoration: none;
+  background-color: #e8e9eb;
+  color: black;
+}
 </style>
--- capstone/vue/src/views/VolunteerApplicationView.vue
+++ capstone/vue/src/views/VolunteerApplicationView.vue
@@ -1,18 +1,141 @@
 <template>
   <body>
-    <div>
-      <volunteer-application-comp></volunteer-application-comp>
+    <div class="home">
+      <div class="logo">
+        <logo-comp></logo-comp>
+      </div>
+      <div class="title" v-if="!mobileView">
+        <title-comp></title-comp>
+      </div>
+      <div class="loginRegister">
+        <login-register-comp></login-register-comp>
+      </div>
+      <div class="nav">
+        <nav-comp></nav-comp>
+      </div>
+      <div class="main">
+        <volunteer-application-comp></volunteer-application-comp>
+      </div>
+      <div class="story">
+        <stories-comp></stories-comp>
+      </div>
+      <div class="feet">
+        <foot-view />
+      </div>
     </div>
   </body>
 </template>
 
 <script>
+import NavComp from "@/components/NavComp.vue";
+import StoriesComp from "@/components/StoriesComp.vue";
+import FootView from "@/views/FootView.vue";
+import LogoComp from "@/components/LogoComp.vue";
+import TitleComp from "@/components/TitleComp.vue";
+import LoginRegisterComp from "@/components/LoginRegisterComp.vue";
 import VolunteerApplicationComp from "@/components/VolunteerApplicationComp.vue";
 
 export default {
-  name: "volunteer-application-view",
+  name: "home",
+  data() {
+    return {
+      mobileView: false,
+    };
+  },
   components: {
+    NavComp,
+    StoriesComp,
+    FootView,
+    LogoComp,
+    TitleComp,
+    LoginRegisterComp,
     VolunteerApplicationComp,
+  },
+  methods: {
+    handleView() {
+      this.mobileView = window.innerWidth <= 450;
+    },
+  },
+  created() {
+    this.handleView();
+    window.addEventListener("resize", this.handleView);
   },
 };
 </script>
+
+
+<style >
+* {
+  background-color: #e0dfd5;
+}
+.logo {
+  grid-area: ga-logo;
+}
+
+.title {
+  grid-area: ga-title;
+}
+
+.loginRegister {
+  grid-area: ga-lr;
+}
+
+.nav {
+  grid-area: ga-nav;
+  border-top: black;
+}
+
+.main {
+  grid-area: ga-main;
+  border-top: black;
+}
+
+.story {
+  grid-area: ga-story;
+  border-top: black;
+}
+
+.feet {
+  grid-area: ga-feet;
+}
+
+.home {
+  display: grid;
+  grid-template-columns: 1fr 3fr 1fr;
+  grid-template-areas:
+    "ga-logo    ga-title  ga-lr"
+    "ga-nav     ga-main   ga-story"
+    "ga-feet    ga-feet   ga-feet";
+  gap: 10px;
+  height: 90vh;
+  background-color: #e0dfd5;
+}
+
+.card * {
+  background-color: aliceblue !important;
+}
+
+@media screen and (max-width: 450px) {
+  .nav {
+    border-style: none;
+    height: 0hv;
+  }
+
+  .logo {
+    padding: 0% 0% 0% 0%;
+    width: 40vw;
+    margin: 10px;
+  }
+
+  .home {
+    width: 100vw;
+    grid-template-columns: 1fr 1fr;
+    grid-template-areas:
+      "ga-logo ga-lr"
+      "ga-nav ga-nav"
+      "ga-main ga-main"
+      "ga-story ga-story"
+      "ga-feet ga-feet";
+  }
+}
+</style> 
